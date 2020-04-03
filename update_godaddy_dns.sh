3b10c6d2e20b001dd06726d8e8796c57d5e9bf68
w <w@xvk3.net>
2020-04-03 20:25:47 +0100

    refactor and bugfix

--- update_godaddy_dns.sh
+++ update_godaddy_dns.sh
@@ -15,7 +15,6 @@ fi
 mydomain="${cfg[0]}"
 myhostname="${cfg[1]}"
 gdapikey="${cfg[2]}"
-logdest="local7.info"
 
 # Attempt to lookup IP address
 myip=`curl -s "https://api.ipify.org"`
@@ -28,7 +27,7 @@ if [[ "$myip" != "$myip2" ]]; then
   myip2_valid=`echo $myip2 | grep -Po "(\d+\.?)*"`
   # If result of the validity check is NULL:
   if [[ -z "$myip_valid" ]]; then
-    # Check the alternate lookup IP for validity
+    # Check the alternate IP for validity
     if [[ -z "$myip2_valid" ]]; then
       # If both IPS are invalid exit
       echo "Unable to lookup IP address"
@@ -40,6 +39,7 @@ if [[ "$myip" != "$myip2" ]]; then
     fi
   fi
 fi
+
 # In the case where both lookup methods return NULL - exit
 if [[ -z "$myip" ]]; then
   echo "Unable to lookup IP address"
@@ -48,13 +48,19 @@ fi
 
 # Execute GET request on the godaddy API
 dnsdata=`curl -s -X GET -H "Authorization: sso-key ${gdapikey}" "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}"`
+
 # Parse response
-gdip=`echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2`
+tmp=`echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2`
+gdip=`echo $tmp | grep -Po "^(\d+\.?)*"`
+if [[ -z "$gdip" ]]; then
+  echo "Bad GoDaddy response, check configuration file"
+  exit 4
+fi
+
 echo "`date '+%Y-%m-%d %H:%M:%S'` - Current External IP is $myip, GoDaddy DNS IP is $gdip"
 
-# Check for success
-if [ "$gdip" != "$myip" -a "$myip" != "" ]; then
+# Does DNS record need updating?
+if [ "$gdip" != "$myip" ]; then
   echo "IP has changed!! Updating on GoDaddy"
   curl -s -X PUT "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}" -H "Authorization: sso-key ${gdapikey}" -H "Content-Type: application/json" -d "[{\"data\": \"${myip}\"}]"
-  logger -p $logdest "Changed IP on ${hostname}.${mydomain} from ${gdip} to ${myip}"
 fi
